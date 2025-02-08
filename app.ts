794530117126bbbe2abc7c93ec198198a836b5ce
theskyworld <2456450687@qq.com>
2025-02-08 23:14:51 +0800

    chore: 修改路由的缓存时间

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ import userRouter from "./routers/userRouter";
 const app = express();
 app.use(cors());
 const cache = require("apicache").middleware;
-app.use(cache("30 minutes"));
+app.use(cache("10 minutes"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
